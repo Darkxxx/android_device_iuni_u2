908ed92e5a3ca74f85615fcc187a9374231bd5c2
Murat Kozan <muratkozan300@gmail.com>
2017-08-24 21:20:40 +0300

    Update u2.mk

--- u2.mk
+++ u2.mk
@@ -216,6 +216,19 @@ PRODUCT_COPY_FILES += \
 # GPS configuration
 PRODUCT_COPY_FILES += \
     device/iuni/u2/gps.conf:system/etc/gps.conf
+    
+# NFC packages
+PRODUCT_PACKAGES += \
+    nfc_nci.bcm2079x.default \
+    NfcNci \
+    Tag
+ 
+ PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
+    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
+    device/ıunı/u2/nfc/libnfc-brcm.conf:system/etc/libnfc-brcm.conf \
+    device/ıunı/u2/nfc/libnfc-brcm-20791b05.conf:system/etc/libnfc-brcm-20791b05.conf \
+    device/ıunı/u2/nfc/nfcee_access.xml:system/etc/nfcee_access.xml
 
 # bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
